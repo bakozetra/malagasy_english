fb1e2930e68e0a02b14ded9f24a13696cd1ad58f
student <Bakozetra>
2021-04-26 10:49:13 +0300

    Created new component and working on the component

---
+++ src/components/SectionHeading/sectionHeading.stories.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import { storiesOf } from '@storybook/react-native';
+import SectionHeading from './sectionHeading';
+
+
+storiesOf('SectionHeading', module)
+    .addDecorator(story => <View style={{ paddingLeft: 23 }}>{story()}</View>)
+    .add('Section heading', () => <SectionHeading text='select a category' />)
