c7196c6ceff00b1f396440f66c9326842a87a20d
student <Bakozetra>
2021-04-26 15:14:38 +0300

    Fixing the name of file and functon

---
+++ src/data/dataUtils.js
@@ -0,0 +1,37 @@
+const categoriesData = require('./categories.json');
+const phrasesData = require('./phrases.json');
+
+export const LANGUAGE_NAMES = {
+  MG: 'mg',
+  EN: 'en',
+};
+
+export const getAllCategoriesNames = lang => {
+  return categoriesData.categories.map(cat => cat.name[lang]);
+};
+
+export const getAllCategoriesIds = () => {
+  return categoriesData.categories.map(cat => cat.id);
+};
+
+export const getPhraseIdsForCategory = catId => {
+  const category = categoriesData.categories.find(cat => cat.id === catId)
+  return (category && category.phrasesIds) || null;
+};
+
+export const getAllPhrases = () => {
+  return phrasesData.phrases;
+};
+
+export const findPhraseCategories = phraseId => {
+  return phrasesData.phrases;
+};
+
+export function findCategoryById(id) {
+  throw 'not implemented!';
+  // TODO implement
+}
+export function findPhraseById(id) {
+  throw 'not implemented!';
+  // TODO implement
+}
