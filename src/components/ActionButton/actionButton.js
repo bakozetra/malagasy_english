46b79f2665bda61bee18418733834368eb93730e
student <Bakozetra>
2021-04-27 15:43:07 +0300

    Adding some styles

--- src/components/ActionButton/actionButton.js
+++ src/components/ActionButton/actionButton.js
@@ -2,11 +2,14 @@ import * as React from 'react';
 import { Text, View, StyleSheet, TouchableHighlight } from 'react-native';
 
 const styles = StyleSheet.create({
+    content: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "flex-end"
+    },
     text: {
         fontSize: 16,
-        position: "absolute",
-        top: 10,
-        right: 10
     },
 })
 
@@ -14,10 +17,12 @@ export default function ActionButton({ text, Svg, color, onPress }) {
     return (
         <View>
             <TouchableHighlight style={styles.pressButton} onPress={onPress}>
-                <Text style={styles.text} color={color}>
-                    {text ? text : ''}
-                    {Svg ? <Svg /> : null}
-                </Text>
+                <View style={styles.content} >
+                    <Text style={styles.text} color={color}>
+                        {text ? text : ''}
+                    </Text>
+                    {Svg ? <Svg marginLeft={10} /> : null}
+                </View>
             </TouchableHighlight>
         </View>
     );
--- src/components/ActionButton/actionButton.stories.js
+++ src/components/ActionButton/actionButton.stories.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { Alert, View } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
 
@@ -10,7 +10,25 @@ import VectorIcon from '../../svg/vector';
 
 storiesOf('ActionButton', module)
     .addDecorator(story => <View>{story()}</View>)
-    .add('Learn icon', () => <ActionButtton text="Learn" color={"red"} Svg={VectorIcon} />)
-    .add('Pick icon', () => <ActionButtton text="Pick" Svg={VectorIcon} />)
-    .add('True icon', () => <ActionButtton text="correct" Svg={TrueIcon} />)
-    .add('Wrong icon', () => <ActionButtton text="wrong" Svg={wrongIcon} />)
+    .add('Learn icon',
+        () =>
+            <ActionButtton
+                text="Learn"
+                color={"red"}
+                Svg={VectorIcon}
+                onPress={() => Alert} />)
+    .add('Pick icon',
+        () =>
+            <ActionButtton
+                text="Pick"
+                Svg={VectorIcon} />)
+    .add('True icon',
+        () =>
+            <ActionButtton
+                text="correct"
+                Svg={TrueIcon} />)
+    .add('Wrong icon',
+        () =>
+            <ActionButtton
+                text="wrong"
+                Svg={wrongIcon} />)
