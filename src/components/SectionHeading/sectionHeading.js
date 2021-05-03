8040c811361bb8fa6aa7f361b0daa47883a332c8
student <Bakozetra>
2021-05-03 09:40:03 +0300

    Changing the name of props and add style

--- src/components/SectionHeading/sectionHeading.js
+++ src/components/SectionHeading/sectionHeading.js
@@ -1,18 +1,19 @@
 import * as React from 'react';
 import { Text, SafeAreaView, StyleSheet } from 'react-native';
 
-export default function SectionHeading({ text }) {
-    console.log(text);
+export default function SectionHeading({ heading }) {
+    console.log(heading);
     const styles = StyleSheet.create({
         text: {
             fontSize: 18,
             lineHeight: 22,
             color: "#111827",
+            paddingBottom: 15,
         }
     })
     return (
         <SafeAreaView >
-            <Text style={styles.text}>{text}</Text>
+            <Text style={styles.text}>{heading}</Text>
         </SafeAreaView>
     );
 }
