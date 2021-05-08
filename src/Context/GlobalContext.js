556d3cd9be7a000bb4453d60b3175d538870904e
student <Bakozetra>
2021-05-08 14:21:49 +0300

    Removed som codes and added two function to handle the phrases text on the four button

--- src/Context/GlobalContext.js
+++ src/Context/GlobalContext.js
@@ -1,15 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import DataCategories from "../data/categories.json"
+import DataPhrases from "../data/phrases.json"
+import { getAllPhrases } from "../data/dataUtils"
+
 
 export const useContextCategories = React.createContext();
 const CategoriesProvider = ({ children }) => {
     const [categories, setCategories] = useState([]);
+    const [Phrases, setPhrases] = useState([]);
+
+    const findPhraseCategories = phraseId => {
+        return Phrases.phrases;
+    };
+
+    const filterPhaseById = (id) => {
+        const filterDataPhraseById = Phrases.phrases.filter(phaseId => phaseId.id === id)
+        return filterDataPhraseById
+    }
+
+
     useEffect(() => {
         setCategories(DataCategories.categories)
-    })
+        setPhrases(DataPhrases)
+    }, [])
+
+
 
     return (
-        <useContextCategories.Provider value={{ categories, setCategories }}>
+        <useContextCategories.Provider value={{ categories, setCategories, Phrases, findPhraseCategories, filterPhaseById }}>
             {children}
         </useContextCategories.Provider>
     )
